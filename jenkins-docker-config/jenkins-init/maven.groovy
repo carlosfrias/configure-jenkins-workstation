7ad04e1b9da6c228763a44b60703e1bad2bb6100
Carlos Frias <friasc@google.com>
2017-04-18 16:48:46 -0400

    Updated to pull maven home using tool

--- jenkins-docker-config/jenkins-init/maven.groovy
+++ jenkins-docker-config/jenkins-init/maven.groovy
@@ -9,7 +9,7 @@ println "$configPrefixMessage: Configuring Maven 3..."
 descriptor = Jenkins.instance.getDescriptorByType(Maven.DescriptorImpl.class)
 if(!descriptor.installations.size()) {
     println "$configPrefixMessage: Creating a Maven Configuration..."
-    installation = new Maven.MavenInstallation("Maven 3", "/usr/share/maven/apache-maven-{{ maven_version }}", null)
+    installation = new Maven.MavenInstallation("Maven 3.3.9", "/usr/share/maven/apache-maven-3.3.9", null)
     descriptor.installations = [ installation ]
     descriptor.save()
     println "$configPrefixMessage: Saved Maven configuration."
