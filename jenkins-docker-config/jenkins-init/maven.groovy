0b6fab329782823ea49cc25bc56dcb37e46740bf
Carlos Frias <friasc@google.com>
2017-05-10 20:08:06 -0400

    Updated files to be more to use variables better.

--- jenkins-docker-config/jenkins-init/maven.groovy
+++ jenkins-docker-config/jenkins-init/maven.groovy
@@ -9,7 +9,7 @@ println "$configPrefixMessage: Configuring Maven..."
 descriptor = Jenkins.instance.getDescriptorByType(Maven.DescriptorImpl.class)
 if(!descriptor.installations.size()) {
     println "$configPrefixMessage: Creating a Maven Configuration..."
-    installation = new Maven.MavenInstallation("Maven 3.3.9", "/usr/share/maven/apache-maven-3.3.9", null)
+    installation = new Maven.MavenInstallation("Maven", "/usr/share/maven/apache-maven-3.3.9", null)
     descriptor.installations = [ installation ]
     descriptor.save()
     println "$configPrefixMessage: Saved Maven configuration."
